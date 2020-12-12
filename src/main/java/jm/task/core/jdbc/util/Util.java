858540c05479d39cdbea828e1315b2073e486b88
Ekaterina <75692122+BuravtsevaEkaterina@users.noreply.github.com>
2020-12-12 20:42:10 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,9 +3,9 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    public static final String URL = "jdbc:mysql://localhost:3306/users?serverTimezone=UTC";
-    public static final String LOGIN = "root";
-    public static final String PASS = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/users?serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASS = "root";
 
     public static Connection connection() throws SQLException, ClassNotFoundException {
         return DriverManager.getConnection(URL, LOGIN, PASS);
